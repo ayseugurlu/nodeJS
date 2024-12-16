2b68bb7e5c9e390f1ba3a0fdfaf0ddaa56d2ab73
ayseugurlu <ayseugurlu9135@gmail.com>
2024-12-16 12:55:03 +0100

    blog app ejs end

---
+++ class-notes/33-Template_BlogApp-2/src/controllers/view/blogPostController.js
@@ -0,0 +1,91 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - BLOG Project with Mongoose
+------------------------------------------------------- */
+
+const BlogPost = require("../../models/blogPostModel");
+const BlogCategory = require("../../models/blogCategoryModel");
+const removeQueryParam = require("../../helpers/removeQueryParam");
+
+// ------------------------------------------
+// BlogPost
+// ------------------------------------------
+
+module.exports = {
+  list: async (req, res) => {
+
+    // list all published posts
+    const data = await res.getModelList(BlogPost, { published: true }, "blogCategoryId");
+    // list all categories
+    const categories = await BlogCategory.find()
+    // List recent 3 posts
+    const recentPosts = await BlogPost.find({ published: true }).sort({ createdAt: "desc" }).limit(3)
+    // Get page details
+    const details = await res.getModelListDetails(BlogPost, { published: true })
+
+    // req.originalUrl
+    // console.log(req.originalUrl);
+    let pageUrl = '';
+    const queryString = req.originalUrl.split('?')[1];
+
+    if (queryString) {
+      pageUrl = removeQueryParam(queryString, 'page')
+    }
+
+    pageUrl = pageUrl ? "&" + pageUrl : "";
+
+
+    res.render('index', { categories, posts: data, recentPosts, details, pageUrl })
+  },
+
+  create: async (req, res) => {
+
+
+    if (req.method == 'POST') {
+      req.body.userId = req.session?.user.id
+      const data = await BlogPost.create(req.body);
+      res.redirect('/blog/post')
+
+    } else {
+
+      const categories = await BlogCategory.find();
+      res.render('postForm', { post: null, categories })
+    }
+
+  },
+
+  read: async (req, res) => {
+    // req.params.postId
+    // const data = await BlogPost.findById(req.params.postId)
+    const data = await BlogPost.findOne({ _id: req.params.postId }).populate("blogCategoryId"); // get Primary Data
+
+    res.render('postRead', { post: data })
+
+  },
+
+  update: async (req, res) => {
+    // const data = await BlogPost.findByIdAndUpdate(req.params.postId, req.body, { new: true }) // return new-data
+
+    if (req.method == 'POST') {
+
+      const data = await BlogPost.updateOne({ _id: req.params.postId }, req.body, { runValidators: true });
+
+      res.redirect('/blog/post')
+
+    } else {
+
+      const post = await BlogPost.findOne({ _id: req.params.postId }).populate("blogCategoryId");
+      const categories = await BlogCategory.find();
+
+      res.render('postForm', { post, categories })
+    }
+
+
+  },
+
+  //Todo -> complete this controller
+  delete: async (req, res) => {
+    const data = await BlogPost.deleteOne({ _id: req.params.postId });
+
+  },
+};
---
+++ class-notes/33-Template_BlogApp-2/src/routes/view/index.js
@@ -0,0 +1,20 @@
+"use strict";
+/* -------------------------------------------------------
+    NODEJS EXPRESS | CLARUSWAY FullStack Team
+------------------------------------------------------- */
+const router = require("express").Router();
+/* ------------------------------------------------------- */
+// ROUTER INDEX:
+
+// URL: /
+
+// blogCategory:
+router.use("/category", require("./blogCategoryRoute"));
+// blogPost:
+router.use("/post", require("./blogPostRoute"));
+// user:
+router.use("/user", require("./userRoute"));
+// auth:
+router.use("/", require("./authRoute"));
+
+module.exports = router;
